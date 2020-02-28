03bfc682407279214a014578a3ed83245a336694
David Knudson <djknudson@icloud.com>
2020-02-28 08:23:13 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,5 +1,9 @@
 def find_element_index(array, value_to_find)
   # Add your solution here
+  # if !array.include?(value_to_find) do
+  #   return nil
+  # end
+   array.index(value_to_find)
 end
 
 def find_max_value(array)
